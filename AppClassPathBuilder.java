9c9795d3780d1970d94f343cd99130cc3533b0e1
yang.yong <2315979845@qq.com>
2018-02-25 15:14:05 +0800

    Java app class path builder

---
+++ AppClassPathBuilder.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Auto build class path where the system(or application) class loader search
+ * classes. The path is a folder which stores all jars.
+ * 
+ * @author YangYong
+ * @version 1.0.0 2018/02/25
+ *
+ */
+public class AppClassPathBuilder {
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Usage:java AppClassPathBuilder jarPath separator");
+			System.out.println("separator:Windows is ; otherwise :");
+			System.exit(1);
+		}
+
+		Path path = Paths.get(args[0]);
+		try (DirectoryStream<Path> entries = Files.newDirectoryStream(path, "*.jar")) {
+			StringBuilder classPath = new StringBuilder();
+
+			for (Path entry : entries) {
+				classPath.append(entry + args[1]);
+			}
+
+			System.out.println(classPath.substring(0, classPath.length() - 1));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
